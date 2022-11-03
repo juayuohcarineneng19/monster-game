67298d32166f0ff1acc88a603c16b364708a07a6
https://github.com/juayuohcarineneng19/ <juayuoh@gmail.com>
2022-11-03 10:56:14 -0700

    adding an atack function

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const attackValue = 10;
+
 let chosenMaxLife = 100
+let currentMonsterHealth = chosenMaxLife;
+let currentPlayerLife = chosenMaxLife;
+
 
 adjustHealthBars(chosenMaxLife);
 
-function attackHandler(params) {
-     const damage = dealMonsterDamage(attackValue)
+function attackHandler() {
+     const damage =  dealMonsterDamage(attackValue)
+     currentMonsterHealth -= damage
 }
 
 attackBtn.addEventListener('click', attackHandler)
