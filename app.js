6e72a26fb157dd30966736306e05d6c31994e7df
https://github.com/juayuohcarineneng19/ <juayuoh@gmail.com>
2022-11-03 21:12:25 -0700

    strong attack functionnality

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const attackValue = 10;
+const strongAttackValue = 17;
 const monsterAttackValue = 14;
 
-let chosenMaxLife = 5
+let chosenMaxLife = 100
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerLife = chosenMaxLife;
 
@@ -24,4 +25,21 @@ function attackHandler() {
      }
 }
 
-attackBtn.addEventListener('click', attackHandler)
+function strongAttackHandler() {
+     const damage =  dealMonsterDamage(strongAttackValue)
+     currentMonsterHealth -= damage;
+     const playerDamage = dealPlayerDamage(monsterAttackValue);
+     currentPlayerHealth =- playerDamage;
+     if (currentMonsterHealth <=0 && currentPlayerHealth > 0){
+          alert ('You won!');
+     } 
+     else if (currentPlayerHealth <=0 && currentMonsterHealth >0){
+          alert('You lost!');
+     }
+     else if (currentPlayerHealth <=0 && currentMonsterHealth  <=0){
+          alert ('You have a draw!')
+     }
+}
+
+attackBtn.addEventListener('click', attackHandler);
+strongAttackBtn.addEventListener('click', strongAttackHandler);
